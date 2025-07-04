22b253fa0c1dc5172cb304b89a049cb79afadde2
kyneccc <121186321+kyneccc@users.noreply.github.com>
2025-07-04 14:01:42 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash 
 
+
+
 apt-get install -y sudo
 #добавление рута в файл sudoers
 echo 'root ALL=(ALL:ALL) ALL' >> /etc/sudoers
 # Создание всех необходимых папок и файлов
-sudo -u test mkdir -p /home/test/Music/playlists
+sudo -u test mkdir -p /home/test/Music/playlist
 sudo -u test mkdir -p /home/test/logs/
-sudo -u test mkdir /home/test/.config/mpd/
 sudo -u test touch /home/test/.config/mpd/db.mpd
 
 # Копирование конфигурационного файла
-cp ./mpd.conf /home/test/.config/mpd/
+cp ./mdp.conf /home/test/.config/mpd/
 
 #установка необходимых пакетов
 apt-get update && apt-get install -y myMPD mpd
 
-sudo -u  test mpd /home/test/.config/mpd/mpd.conf
-systemctl start mympd                          
+sudo -u  test mpd /home/test/.config/mpd/db.mpd
+systemctl start mympd
